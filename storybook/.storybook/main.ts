9562a65d8d7b6e8beb0ca12e231e598fcfdebbcb
Michael Shilman <shilman@users.noreply.github.com>
2025-10-25 11:03:59 +0800

    chore: upgrade to storybook 10

--- storybook/.storybook/main.ts
+++ storybook/.storybook/main.ts
@@ -1,6 +1,11 @@
+// This file has been automatically migrated to valid ESM format by Storybook.
+import { fileURLToPath } from 'node:url';
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
-import path from 'path';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const Component = process.env.M;
 
